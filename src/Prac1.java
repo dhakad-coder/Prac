83114134c99536a0bb1e5cf21333260e6912b2ff
Raman <extern.raman.ranjan@elektrobit.com>
2022-06-18 11:15:40 +0530

    nn

---
+++ src/Prac1.java
@@ -0,0 +1,133 @@
+/*
+public class Prac1 {
+    static int m = 150; // static variable
+    void method(){
+        int n = 100; // local variable
+    }
+    public static void main(String[] args){
+        int data = 200; //instance variable
+    }
+}// end of class
+*/
+
+/*
+public class Prac1{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c= a+b;
+        System.out.println(c);
+    }
+}
+*/
+
+/*
+public class Prac1{
+    public static void main(String[] args){
+        String s1 = "Elektrobit"; //creating string by Java string literal
+        char ch[] = {'s', 't', 'r', 'i', 'n', 'g', 's'};
+        String s2 = new String(ch);//Converting char array to string
+        String s3 = new String("Elektrobit India"); //creating Java string by new keyword
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+}
+ */
+
+/*
+public class Prac1{
+    public static void main(String[] args){
+        //loop of i
+        for(int i = 1; i<=3; i++){
+        //loop of j
+        for(int j=1;j<=3; j++){
+            System.out.println(i+" "+j);
+        }//end of i
+        }//end of j
+    }
+}
+ */
+
+/*
+public class Prac1{
+    public static void main(String[] args){
+        for(int i =1;i<=20;i++){
+        for(int j=1; j<=i;j++){
+            System.out.print(" * ");
+        }
+            System.out.println();//new line
+        }
+    }
+}
+ */
+
+/*
+public class Prac1 {
+    public static void main(String[] args){
+        //Declaring an array
+    int arr[] = {12,23,44,56,78, 98, 118, 128, 148};
+    //printing array using for each - loop
+    for(int i:arr){
+        System.out.println(i);
+    }
+    }
+}
+ */
+
+/*
+public class Prac1 {
+    public static void main(String[] args){
+        int i = 2;
+        while(i<=20){
+            System.out.println(i);
+        i++;
+        }
+
+    }
+}
+ */
+
+/*
+public class Prac1 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 20);
+    }
+}
+ */
+
+/*
+public class Prac1{
+    public static void main(String[] args){
+        //Declaring a variable for switch expression
+        int number = 20;
+        //Switch Expression
+        switch(number){
+            //Case statements
+            case 10:
+                System.out.println("10");
+            break;
+            case 20:
+                System.out.println("20");
+            break;
+            case 30:
+                System.out.println("30");
+            break;
+            //Default case statement
+            default:
+                System.out.println("Not in 10, 20 or 30");
+
+        }
+    }
+}
+ */
+
+
+
+
